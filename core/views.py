3ef83de472d42b2b2fe872d8eb12a0634a334389
alliance <alliancekubwayo@gmail,com>
2026-03-06 21:47:20 +0200

    feat: affichage des cotisations par membre dans le detail groupe

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from .models import Membre, Groupe, Cycle
+from .models import Membre, Groupe, Cycle, Cotisation
 
 
 @login_required
@@ -14,5 +14,10 @@ def liste(request):
 def detail_groupe(request, pk):
   groupe = get_object_or_404(Groupe, id=pk)
   cycle = Cycle.objects.filter(groupe=groupe, statut='en_cours').first()
+  cotisations = Cotisation.objects.filter(cycle=cycle)
   membres = groupe.membres.all()
-  return render(request, 'core/detail_groupe.html', {'membres': membres, 'cycle': cycle})
+  cotisation_membre = []
+  for membre in membres:
+    cotisation = cotisations.filter(membre=membre).first()
+    cotisation_membre.append((membre, cotisation))
+  return render(request, 'core/detail_groupe.html', {'membres': membres, 'cycle': cycle, 'cotisation_membre': cotisation_membre})
