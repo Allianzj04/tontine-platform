4031a3d5fcf53d1d269832f2873d78222baf41f7
alliance <alliancekubwayo@gmail,com>
2026-03-07 09:36:34 +0200

    feat: enregistrement des paiements de cotisation

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Membre, Groupe, Cycle, Cotisation
@@ -20,4 +20,11 @@ def detail_groupe(request, pk):
   for membre in membres:
     cotisation = cotisations.filter(membre=membre).first()
     cotisation_membre.append((membre, cotisation))
+  if request.method == "POST":
+    cotisation_id = request.POST.get("cotisation_id")
+    cotisation_paye = get_object_or_404(Cotisation, id=cotisation_id)
+    cotisation_paye.statut = "paye"
+    cotisation_paye.save()
+    return redirect('detail_groupe', pk)
+
   return render(request, 'core/detail_groupe.html', {'membres': membres, 'cycle': cycle, 'cotisation_membre': cotisation_membre})
