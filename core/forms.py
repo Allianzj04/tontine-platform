8903ed613df5d9b01e05e54a65e6e6cc5b0294a2
alliance <alliancekubwayo@gmail,com>
2026-03-09 14:32:06 +0200

    feat: add GroupeForm for groupe creation

--- core/forms.py
+++ core/forms.py
@@ -1,12 +1,18 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Groupe
+
 
 class InscriptionForm(UserCreationForm):
   nom = forms.CharField(max_length=100)
   prenom = forms.CharField(max_length=100)
   email = forms.EmailField()
-
   class Meta:
     model = User
     fields = ['username', 'password1', 'password2']
+
+class GroupeForm(forms.ModelForm):
+  class Meta:
+    model = Groupe
+    fields = ['nom', 'montant_cotisation']
