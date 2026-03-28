8cf1e526e1c4e6d51051598537de4f2474ef1aaf
alliance <alliancekubwayo@gmail,com>
2026-03-28 09:50:01 +0200

    feat: add member__id and calculated columns to analytics script

--- analytics.py
+++ analytics.py
@@ -10,6 +10,7 @@ from core.models import Contribution
 
 data = Contribution.objects.all().values(
   'id',
+  'member__id',
   'member__first_name',
   'member__last_name',
   'cycle__group__name',
@@ -21,8 +22,11 @@ data = Contribution.objects.all().values(
 df = pd.DataFrame(data)
 # print(df)
 
+df['amount_due'] = np.where(df['status'] == 'paid', 0, df['cycle__group__amount'])
+df['amount_paid'] = np.where(df['status'] == 'paid', df['cycle__group__amount'], 0)
+
 df_rate = df.groupby('cycle__group__name')['status'].value_counts(normalize=True)*100
-print(df_rate)
+# print(df_rate)
 
 df_defaulters = df[df['status'] == 'unpaid']
 # print(df_defaulters)
@@ -31,8 +35,19 @@ df_paid = df[df['status'] == 'paid']
 total_groups = df_paid.groupby('cycle__group__name')['cycle__group__amount'].sum()
 # print(total_groups)
 
-df['amount_due'] = np.where(df['status'] == 'paid', 0, df['cycle__group__amount'])
-df['amount_paid'] = np.where(df['status'] == 'paid', df['cycle__group__amount'], 0)
-# print(df[['member__name', 'status', 'cycle__group__amount', 'amount_due', 'amount_paid']].tail(10))
+rate = df.groupby('member__id')['status'].value_counts(normalize=True)*100
+rate = rate.unstack(fill_value=0)
+df_rate_member = rate['paid'].reset_index()
+df_names = df[['member__id', 'member__first_name', 'member__last_name']].drop_duplicates()
+df_rate_member = df_rate_member.merge(df_names, on='member__id')
+df_rate_member = df_rate_member.rename(columns={'paid': 'reliability_rate'})
+df_rate_member = df_rate_member.sort_values('reliability_rate', ascending=False)
+print(df_rate_member.columns.tolist())
+print(df_rate_member)
+r = (df_rate_member['reliability_rate'] == 0).value_counts()
+print(r)
 
-df.to_csv('contributions_analytics.csv', index=False)
+df_financial = pd.DataFrame()
+df_financial['amount_expected'] = df.groupby('cycle__group__name')['cycle__group__amount'].sum()
+df_financial['amount_paid'] = df.groupby('cycle__group__name')['amount_paid'].sum()
+df_financial['amount_missing'] = df_financial['amount_expected'] - df_financial['amount_paid']
