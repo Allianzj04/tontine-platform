b7e25b0823f7c946822413ba5fa1dcbe92f7361d
alliance <alliancekubwayo@gmail,com>
2026-04-02 10:06:56 +0200

    refactor: change name of fastapi secret key

--- api/auth.py
+++ api/auth.py
@@ -5,7 +5,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("FASTAPI_SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
