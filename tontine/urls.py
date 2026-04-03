83c7c1b21b4fa83da67a34568c969822adc43f1d
alliance <alliancekubwayo@gmail,com>
2026-04-03 11:58:33 +0200

    fix: add urls from core

--- tontine/urls.py
+++ tontine/urls.py
@@ -21,5 +21,6 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(pattern_name='login')),
+    path('', include('core.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
